63231894d767bad5f800a91cc49db96bf9fe82ee
Konrad Scherer <Konrad.Scherer@windriver.com>
2014-09-10 18:31:29 -0700

    Add build script to make building rpm easier
    Signed-off-by: Konrad Scherer <Konrad.Scherer@windriver.com>

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if [ ! -f SOURCES/apache-activemq-5.7.0-bin.tar.gz ]; then
+    (
+        cd SOURCES
+        wget https://archive.apache.org/dist/activemq/apache-activemq/5.7.0/apache-activemq-5.7.0-bin.tar.gz
+    )
+fi
+
+mkdir -p BUILD RPMS SRPMS
+
+rpmbuild -D '%_topdir %(echo $PWD)' -ba SPECS/activemq.spec
